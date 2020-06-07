0b72f8c1098544f17431069fd8b5da64d1758a2b
Gustavo Zambonin <gzmbnn@gmail.com>
2020-06-07 02:20:22 -0300

    all: adapted figures and tables to thesis * update parameters, axis names, and key size calculations considering   homogeneous polynomials and equivalent linear maps

--- tables/key_sizes.py
+++ tables/key_sizes.py
@@ -24,41 +24,25 @@ def parse_param(args):
     return p
 
 
-def valid_param(args):
+def public_key_size(
+    args,  # instance arguments (q, v_1, o_1, ..., o_u)
+    variant="Classic",  # usual, Cyclic (2010), LRS2, Cyclic (NIST)
+    convert=True,  # output count in bytes instead of field elements
+    homogeneous=True,  # use homogeneous quadratic polynomials only
+):
     p = parse_param(args)
-    alpha = 1.5
-    l = 128
+    pk = p["m"] * (p["n"] + 1) * (p["n"] + 2) / 2
+    blocks = sum(p[f"o{k + 1}"] * p[f"D{k + 1}"] for k in range(p["u"]))
 
-    return all(
-        (
-            all(i > 0 for i in p.values()),
-            p["m"] >= alpha * p[f"o{p['u']}"],
-            p["n"] >= p["m"],
-            p["m"] >= 10,
-            p["v1"] >= l / log2(p["q"]) - 1,
-            p[f"o{p['u']}"] >= l / log2(p["q"]),
-            p["n"] - 2 * p[f"o{p['u']}"] >= l / log2(p["q"]) + 1,
-            p["n"] >= ceil(5 / 3 * (p["m"] - 1)),
-        )
-    )
+    if variant == "Cyclic":
+        pk += p[f"D{p['u']}"] - blocks
+    elif variant == "LRS2":
+        pk += p["m"] - blocks
+    elif variant == "nCyclic":
+        pk -= blocks
 
-
-def public_key_size(args, variant="classic", convert=False):
-    def cyclic():
-        tmp1 = p[f"D{p['u']}"] + p["m"] * ((p["n"] + 1) * (p["n"] + 2)) / 2
-        tmp2 = sum(p[f"o{k + 1}"] * p[f"D{k + 1}"] for k in range(p["u"]))
-        return tmp1 - tmp2
-
-    def lrs2():
-        tmp1 = p["m"] * ((p["n"] + 1) * (p["n"] + 2) + 2) / 2
-        tmp2 = sum(p[f"o{k + 1}"] * p[f"D{k + 1}"] for k in range(p["u"]))
-        return tmp1 - tmp2
-
-    def classic():
-        return p["m"] * (p["n"] + 1) * (p["n"] + 2) / 2
-
-    p = parse_param(args)
-    pk = locals()[variant.lower()]()
+    if homogeneous:
+        pk -= p["m"] * (p["n"] + 1)
 
     if convert:
         if p["q"] == 16:
@@ -69,28 +53,46 @@ def public_key_size(args, variant="classic", convert=False):
     return pk
 
 
-def private_key_size(args, eta=False, convert=False):
+def private_key_size(
+    args,  # instance arguments (q, v_1, o_1, ..., o_u)
+    eta=False,  # fix v_1 terms in the central map
+    convert=True,  # output count in bytes instead of field elements
+    linear=False,  # drop constant terms in affine maps
+    equivalent=False,  # use "equivalent key" lateral maps (implies linear)
+    homogeneous=True,  # use homogeneous quadratic polynomials only
+):
     p = parse_param(args)
+    sk = 0
 
-    sk = p["n"] ** 2 + p["n"]
-    if p["u"] > 1:
-        sk += p["m"] ** 2 + p["m"]
+    if p["u"] <= 2 and equivalent:
+        if p["u"] == 2:
+            sk += p["o1"] * p["o2"] + p["v1"] * p["o1"] + p["v2"] * p["o2"]
+        else:
+            sk += p["v1"] * p["o1"]
+    else:
+        sk += p["n"] ** 2 + p["n"]
+        if p["u"] > 1:
+            sk += p["m"] ** 2 + p["m"]
+
+        if linear:
+            sk -= p["n"] + p["m"]
 
     if eta:
         v_1 = p["v1"]
         for k in range(1, p["u"] + 1):
             v_k, o_k = p[f"v{k}"], p[f"o{k}"]
             sk += o_k * (
-                ((v_k - v_1) ** 2 + (v_k - v_1)) / 2
-                + (v_k - v_1) * o_k
-                + (p[f"v{k + 1}"] - v_1)
-                + 1
+                ((v_k - v_1) ** 2 + (v_k - v_1)) / 2 + (v_k - v_1) * o_k
             )
+            if not homogeneous:
+                sk += o_k * ((p[f"v{k + 1}"] - v_1) + 1)
         sk += v_1
     else:
         for k in range(1, p["u"] + 1):
             v_k, o_k = p[f"v{k}"], p[f"o{k}"]
-            sk += o_k * ((v_k ** 2 + v_k) / 2 + v_k * o_k + p[f"v{k + 1}"] + 1)
+            sk += o_k * ((v_k ** 2 + v_k) / 2 + v_k * o_k)
+            if not homogeneous:
+                sk += o_k * (p[f"v{k + 1}"] + 1)
 
     if convert:
         if p["q"] == 16:
@@ -103,8 +105,8 @@ def private_key_size(args, eta=False, convert=False):
 
 def prk_wrapper(args):
     return (
-        private_key_size(args, eta=False, convert=True),
-        private_key_size(args, eta=True, convert=True),
+        private_key_size(args, eta=False, equivalent=True),
+        private_key_size(args, eta=True, equivalent=True),
     )
 
 
@@ -121,26 +123,58 @@ def table_1():
         "VI-b": (31, 84, 56, 56),
     }
 
-    petzoldt_612 = {
-        "P-080": (256, 17, 17, 9),
-        "P-100": (256, 26, 22, 21),
-        "P-128": (256, 36, 28, 15),
+    petzoldt_98_612 = {
+        "P-080": (256, 17, 13, 13),
+        "P-100": (256, 26, 16, 17),
+        "P-128": (256, 36, 21, 22),
         "P-192": (256, 63, 46, 22),
         "P-256": (256, 85, 63, 30),
     }
 
-    t1_line = "{:<6} & {:<33} & {:>4} & {:>4} & {:>8.0f} & {:>7.0f} & ${:>+6.2f}\\%$ \\\\"
+    t1_line = (
+        "{:<6} & {:<33} & {:>4} & {:>4} & {:>8.0f} & {:>7.0f} "
+        "&  ${:>+6.2f}\\%$ \\\\\n"
+    )
     param_fmt = "$(\\mathbb{{F}}_{{{:>3}}}, {:>2}, {:>2}, {:>2})$"
 
-    for name, param in sorted(nist.items()) + sorted(petzoldt_612.items()):
-        old_prk, eta_prk = prk_wrapper(param)
+    contents = ""
+    for name, param in sorted(nist.items()) + sorted(petzoldt_98_612.items()):
         n, m = sum(param[1:]), sum(param[2:])
-        diff = 100 * -(1 - eta_prk / old_prk)
-        print(
-            t1_line.format(
-                name, param_fmt.format(*param), n, m, old_prk, eta_prk, diff
-            )
+        size, new_size = prk_wrapper(param)
+        diff = 100 * -(1 - new_size / size)
+        contents += t1_line.format(
+            name, param_fmt.format(*param), n, m, size, new_size, diff
         )
+
+    return contents
+
+
+def helper_table_2(params, variants):
+    t2_line = (
+        "{:<23} & {:<8} & {:>25} & {:>25} & {:>8.0f} &  ${:>+6.2f}\\%$ \\\\\n"
+    )
+    multi = "\\multirow{{{}}}{{*}}".format(len(variants))
+
+    contents = ""
+    for name, param in params.items():
+        sk, new_sk = prk_wrapper(param)
+        pk = public_key_size(param)
+
+        for var in variants:
+            new_pk = public_key_size(param, variant=var)
+            diff = 100 * -(1 - (new_sk + new_pk) / (sk + pk))
+            sk_m, new_sk_m, name_m = [""] * 3
+
+            if var == "Classic":
+                name_m = "{}{{{:<5}}}".format(multi, name)
+                sk_m = "{}{{{:>7.0f}}}".format(multi, sk)
+                new_sk_m = "{}{{{:>7.0f}}}".format(multi, new_sk)
+
+            contents += t2_line.format(
+                name_m, var, sk_m, new_sk_m, new_pk, diff,
+            )
+
+    return contents
 
 
 def table_2():
@@ -150,39 +184,20 @@ def table_2():
         "P-128": (256, 36, 21, 22),
     }
 
-    variants = ["Classic", "Cyclic", "LRS2"]
-    t2_line = "{:<23} & {:<50} & {:>8} & {:>24} & {:>23} & {:>7.0f} & ${:>+6.2f}\\%$ \\\\"
-    param_fmt = "$(\\mathbb{{F}}_{{{:>3}}}, {:>2}, {:>2}, {:>2})$"
-    multirow = "\\multirow{{{}}}{{*}}".format(len(variants))
+    nist_round2 = {
+        "I-a": (16, 32, 32, 32),
+        "III-c": (256, 68, 36, 36),
+        "V-c": (256, 92, 48, 48),
+    }
 
-    for name, param in sorted(petzoldt_98.items()):
-        old_prk, eta_prk = prk_wrapper(param)
-        old_puk = public_key_size(param)
-        for var in variants:
-            new_puk = public_key_size(param, variant=var)
-            pair_diff = 100 * -(1 - (eta_prk + new_puk) / (old_prk + old_puk))
-            old_prk_multi, eta_prk_multi, param_multi, name_multi = [""] * 4
-            if var == "Classic":
-                name_multi = "{}{{{:<5}}}".format(multirow, name)
-                param_multi = "{}{{{:<32}}}".format(
-                    multirow, param_fmt.format(*param)
-                )
-                old_prk_multi = "{}{{{:>6.0f}}}".format(multirow, old_prk)
-                eta_prk_multi = "{}{{{:>5.0f}}}".format(multirow, eta_prk)
-            print(
-                t2_line.format(
-                    name_multi,
-                    param_multi,
-                    var,
-                    old_prk_multi,
-                    eta_prk_multi,
-                    new_puk,
-                    pair_diff,
-                )
-            )
+    contents = ""
+    contents += helper_table_2(petzoldt_98, ["Classic", "Cyclic", "LRS2"])
+    contents += helper_table_2(nist_round2, ["Classic", "nCyclic"])
+
+    return contents
 
 
 if __name__ == "__main__":
-    table_1()
-    print()
-    table_2()
+    # does not consider seed sizes if PRNGs are used
+    print(table_1())
+    print(table_2())
